c72d30d9701f41a90d977be27543982769850243
Neozaelks <hozaelks@gmail.com>
2022-01-03 17:30:13 +0100

    Added /city claim (single|square|circle|auto) boilerplate. Finished /city claim single.

--- src/main/java/fr/lapalmeraiemc/polis/commands/ClaimChunkCommands.java
+++ src/main/java/fr/lapalmeraiemc/polis/commands/ClaimChunkCommands.java
@@ -2,16 +2,96 @@ package fr.lapalmeraiemc.polis.commands;
 
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.CommandCompletion;
 import co.aikar.commands.annotation.Subcommand;
+import co.aikar.commands.annotation.Values;
+import fr.lapalmeraiemc.polis.enums.Messages;
+import fr.lapalmeraiemc.polis.enums.Roles;
+import fr.lapalmeraiemc.polis.models.ClaimsManager;
+import fr.lapalmeraiemc.polis.models.Member;
+import fr.lapalmeraiemc.polis.models.MemberManager;
+import fr.lapalmeraiemc.polis.utils.Config;
+import fr.lapalmeraiemc.polis.utils.Localizer;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.Chunk;
 import org.bukkit.entity.Player;
+
+import javax.inject.Inject;
+import java.util.Objects;
 
 
 @CommandAlias("city|ville")
 public class ClaimChunkCommands extends BaseCommand {
 
+  @Inject private Config        config;
+  @Inject private MemberManager memberManager;
+  @Inject private Localizer     localizer;
+  @Inject private ClaimsManager claimsManager;
+  @Inject private Economy       economy;
+
   @Subcommand("claim")
-  public void create(Player player, String mode) {
-    throw new UnsupportedOperationException();
+  @CommandCompletion("single|square|circle|auto")
+  public void claim(Player player, @Values("single|square|circle|auto") String mode) {
+
+    boolean isACityMember = memberManager.isAlreadyCityMember(player.getUniqueId());
+    if(!isACityMember){
+      localizer.sendMessage(player, Messages.CITY_CLAIM_NOT_A_CITY_MEMBER);
+      return;
+    }
+
+    Member member = memberManager.get(player.getUniqueId());
+    if(member.getRole() == Roles.HELPER){
+      localizer.sendMessage(player, Messages.CITY_CLAIM_NO_PERMISSION);
+      return;
+    }
+
+    if(Objects.equals(mode, "single")){
+      Chunk chunkToClaim = player.getChunk();
+      if(claimsManager.hasChunkBeenClaimed(chunkToClaim)){
+        localizer.sendMessage(player, Messages.CITY_CLAIM_CHUNK_ALREADY_CLAIMED);
+        return;
+      }
+
+      long nearestCityID = claimsManager.getNearestCity(chunkToClaim).getId();
+      long memberCityID = member.getCityId();
+
+      if(nearestCityID != memberCityID){
+        localizer.sendMessage(player, Messages.CITY_CLAIM_TOO_CLOSE);
+        return;
+      }
+
+      double distanceToNearestOriginSquared = claimsManager.getDistanceSquaredToNearestOrigin(chunkToClaim);
+      double maxDistanceFromOrigin = config.getMaxClaimDistanceSquared();
+
+      if(distanceToNearestOriginSquared >= maxDistanceFromOrigin){
+        localizer.sendMessage(player, Messages.CITY_CLAIM_TOO_FAR_AWAY);
+      }
+      //after all those checks, the point is legal. Starting the payment process.
+
+      long numberOfCityClaims = claimsManager.getClaimCount(memberCityID);
+      double claimPrice = claimsManager.getClaimPrice(numberOfCityClaims);
+
+      Confirmation.prompt(player,
+                          localizer.getColorizedMessage(Messages.CITY_CLAIM_FEE_PROMPT, claimPrice),
+                          () -> onConfirmSingle(player, claimPrice, memberCityID, chunkToClaim),
+                          () -> localizer.sendMessage(player, Messages.CITY_CLAIM_CANCEL));
+
+    }
+    if(Objects.equals(mode, "square")){
+      throw new UnsupportedOperationException();
+    }
+    if(Objects.equals(mode, "circle")){
+      throw new UnsupportedOperationException();
+    }
+    if(Objects.equals(mode, "auto")){
+      throw new UnsupportedOperationException();
+    }
+  }
+
+  private void onConfirmSingle(Player player, double claimPrice, long cityId, Chunk chunk){
+    economy.withdrawPlayer(player, claimPrice);
+    claimsManager.claim(cityId, chunk);
+    localizer.sendMessage(player, Messages.CITY_CLAIM_CONFIRM);
   }
 
 }
